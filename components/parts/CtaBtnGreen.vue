7f74790f93d7067910ad662c557c3a23ab1c61bc
Yuta Toyokawa <kppg42@gmail.com>
2022-11-24 00:29:55 +0900

    緑ボタンエリアの実装

--- components/parts/CtaBtnGreen.vue
+++ components/parts/CtaBtnGreen.vue
@@ -1,31 +1,36 @@
 <template lang="pug">
 .cta-btn-green
-  .bubble-orange
-    .bubble-text まずは
   .main-text
     span.emphasis-orange 無料
     span 体験
-  .bubble-white-left
-    .bubble-text
-      p 体組成計で
-      p.emphasis-orange 今のカラダの傾向
-      p がわかる
-  .bubble-white-center
-    .bubble-text
-      p カウンセリングで
-      p.emphasis-orange アナタに最適な
-      p
-        span.emphasis-orange プラン
-        span がわかる
-  .bubble-white-right
-    .bubble-text
-      p トレーニング
-      p
-        span 体験で
-        span.emphasis-orange LIXAの良さ
-      p がわかる
+    .bubble-orange
+      img.bubble-image(src="@/assets/images/bubble_orange.png")
+      .bubble-text まずは
+  .bubble-area
+    .bubble-white
+      img.bubble-image(src="@/assets/images/bubble_white_left.png")
+      .bubble-text
+        p 体組成計で
+        p.emphasis-orange 今のカラダの傾向
+        p がわかる
+    .bubble-white
+      img.bubble-image(src="@/assets/images/bubble_white_center.png")
+      .bubble-text
+        p カウンセリングで
+        p.emphasis-orange アナタに最適な
+        p
+          span.emphasis-orange プラン
+          span がわかる
+    .bubble-white
+      img.bubble-image(src="@/assets/images/bubble_white_right.png")
+      .bubble-text
+        p トレーニング
+        p
+          span 体験で
+          span.emphasis-orange LIXAの良さ
+        p がわかる
   .btn-box
-    .btn-area
+    .btn-area.mb-3
       .first-text
         span.emphasis-yellow 無料体験
         span の
@@ -36,24 +41,75 @@
           span.emphasis-md 30
           span 秒
       .triangle-right.ms-auto
-    .d-flex.align-items-center
-      img
-      span 相談だけでもOK！
-    .d-flex.align-items-center
-      img
-      span 無理な勧誘は一切行いません！
+    .check-area.d-flex.justify-content-center
+      div
+        .check-list.d-flex.align-items-center.mb-2
+          img.check.me-2(src="@/assets/images/check.png")
+          span.text 相談だけでもOK！
+        .check-list.d-flex.align-items-center
+          img.check.me-2(src="@/assets/images/check.png")
+          span.text 無理な勧誘は一切行いません！
 </template>
 
 <style lang="sass" scoped>
 .cta-btn-green
   text-align: center
   letter-spacing: 0.05em
+  background-color: #FFE7B8
+  padding: 60px 0 30px
   .main-text
     font-size: 48px
     font-weight: bold
+    position: relative
+    .bubble-orange
+      position: absolute
+      top: -58px
+      left: 50px
+      .bubble-image
+        filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.25))
+        height: 50px
+        width: auto
+      .bubble-text
+        color: white
+        font-size: 14px
+        font-weight: bold
+        width: 100%
+        position: absolute
+        top: 28px
+        left: 50%
+        transform: translateX(-50%)
   .emphasis-orange
     color: $emphasis-orange
+  .bubble-area
+    display: flex
+    justify-content: center
+    .bubble-white
+      position: relative
+      z-index: 1
+      &:nth-child(2)
+        margin: 0 3px
+      .bubble-image
+        width: 120px
+        height: auto
+      .bubble-text
+        font-size: 12px
+        font-weight: bold
+        width: 100%
+        position: absolute
+        bottom: 10px
+        left: 50%
+        transform: translateX(-50%)
+        p
+          padding: 0
+          margin: 0
   .btn-box
+    width: 350px
+    padding: 45px 0 20px
+    background-color: white
+    border-radius: 10px
+    margin: -30px auto 0
+    filter: drop-shadow(0px 8px 4px rgba(0, 0, 0, 0.25))
+    z-index: 1
     .btn-area
       background: linear-gradient(180deg, #9FF05F 0%, #1BAC3B 33.33%)
       width: 320px
@@ -62,6 +118,7 @@
       display: flex
       align-items: center
       filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))
+      margin: 0 auto
       .first-text
         text-align: left
         font-size: 14px
@@ -102,4 +159,11 @@
         border-left: 12px solid white
         filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.25))
         order: 3
+    .check-list
+      .check
+        height: 16px
+        width: auto
+      .text
+        font-size: 14px
+        font-weight: bold
 </style>
