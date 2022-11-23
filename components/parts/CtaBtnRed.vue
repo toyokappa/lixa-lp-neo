f5af9a485c5cda6037e6c4553baca6dc52abddca
Yuta Toyokawa <kppg42@gmail.com>
2022-11-23 23:05:38 +0900

    CTAボタンの最終調整

--- components/parts/CtaBtnRed.vue
+++ components/parts/CtaBtnRed.vue
@@ -14,17 +14,20 @@
           div
             span.emphasis-md 30
             span 秒で完了！
+    .instructor-photo
   .bubble-area
-    .bubble-text 無理な勧誘は一切なし！相談だけでもOK！
+    .bubble-text 無理な勧誘は一切ナシ！相談だけでもOK！
 </template>
 
 <style lang="sass" scoped>
 .cta-btn-red
   text-align: center
+  letter-spacing: 0.05em
   .btn-area
     width: 340px
     height: 110px
     background: linear-gradient(180deg, #FF7B7B 0%, #F43939 44.27%, #F43939 100%)
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))
     border-radius: 10px
     position: relative
     margin: 0 auto
@@ -62,13 +65,23 @@
         top: 70px
         right: 50%
         transform: translateX(50%)
+    .instructor-photo
+      width: 70px
+      height: 70px
+      background-color: $base-grey
+      border: 3px solid $accent-color
+      border-radius: 50%
+      position: absolute
+      bottom: -8px
+      left: -8px
   .bubble-area
+    font-size: 15px
     font-weight: bold
     display: inline-block
     padding: 5px 10px
     border: 2px solid black
-    border-radius: 5px
-    margin: 15px 0 0
+    border-radius: 10px
+    margin: 20px 0 0
     position: relative
     &:before, &:after
       border: solid transparent
@@ -76,7 +89,7 @@
       bottom: 100%
       content: ""
     &:before
-      left: 22px
+      left: 16px
       border-top-width: 6px
       border-bottom-width: 6px
       border-left-width: 3px
@@ -84,7 +97,7 @@
       border-bottom-color: white
       z-index: 2
     &:after
-      left: 20px
+      left: 14px
       border-top-width: 10px
       border-bottom-width: 10px
       border-left-width: 5px
