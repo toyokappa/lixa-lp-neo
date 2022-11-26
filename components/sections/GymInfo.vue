1ed22a1646ebe11d45c40a912eb4a39de9bf2545
Yuta Toyokawa <kppg42@gmail.com>
2022-11-26 16:06:44 +0900

    店舗情報のデザイン修正

--- components/sections/GymInfo.vue
+++ components/sections/GymInfo.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
 section#gymInfo
-  .container.py-3
+  .container
     .section-title.text-center.mb-3 店舗情報
     .title 営業時間
     .content.mb-3 7:00〜22:00
     .title 住所
-    .content.mb-3
+    .content.mb-4
       p 〒371-0842
       p 群馬県前橋市下石倉町12-4
       p AZBL BOXを利用
@@ -17,11 +17,16 @@ section#gymInfo
 #gymInfo
   color: white
   background-color: $dark-color
+  padding: 30px 0 0
   .section-title
     font-size: 20px
     font-weight: bold
   .title
+    font-size: 14px
     font-weight: bold
+    margin-bottom: 5px
+  .content
+    font-size: 14px
   .caption
     font-size: 12px
   .map
