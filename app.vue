3f0312ee879d978287970e9532203e1a2ef59276
Yuta Toyokawa <kppg42@gmail.com>
2022-12-08 20:14:49 +0900

    プライバシーポリシーの作成

--- app.vue
+++ app.vue
@@ -8,4 +8,18 @@ parts-global-footer
 p
   padding: 0
   margin: 0
+.contract
+  padding: 1rem 0
+  section
+    margin-bottom: 2rem
+  h1
+    font-size: 2.5rem
+    font-weight: bold
+  h2
+    font-size: 1.5rem
+    font-weight: bold
+  p
+    margin-bottom: 1.5rem
+  li
+    margin-bottom: 0.5rem
 </style>
