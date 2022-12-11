a12fcc171fbcb5a97fd383b52fc2ed6b59bb2cd0
Yuta Toyokawa <kppg42@gmail.com>
2022-12-11 19:11:44 +0900

    フッターの追加

--- components/parts/global/Footer.vue
+++ components/parts/global/Footer.vue
@@ -1,5 +1,12 @@
 <template lang="pug">
 footer.py-2
+  ul.footer-nav
+    li
+      nuxt-link(to="/terms") 利用規約
+    li
+      nuxt-link(to="/policy") プライバシーポリシー
+    li
+      nuxt-link(to="/tokusho") 特定商取引法に基づく表示
   .text-center &copy; LIXA 2022
 </template>
 
@@ -10,4 +17,14 @@ footer
   letter-spacing: 1px
   color: white
   background-color: $dark-color
+  .footer-nav
+    list-style: none
+    text-align: center
+    margin: 0
+    padding: 10px 0
+    li
+      margin-bottom: 5px
+      a
+        color: inherit
+        text-decoration: none
 </style>
